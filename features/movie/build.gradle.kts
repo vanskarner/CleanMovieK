3d41fd6819f016134757268b9987f2ff9b715137
Vanskarner <vanskarner@gmail.com>
2024-09-09 20:04:01 -0500

    movie module added

--- features/movie/build.gradle.kts
+++ features/movie/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.vanskarner.cleanmoviek"
+    namespace = "com.vanskarner.movie"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.vanskarner.cleanmoviek"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,13 +33,10 @@ android {
 }
 
 dependencies {
-    //main
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":features:movie"))
-
-    //test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "CleanMovieK"
 include(":app")
+include(":features:movie")
